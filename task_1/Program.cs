2f8831098f3f743dd9672a14cb45dae622096fba
MariaMagdalina <metodist_rmmc@mail.ru>
2022-09-10 23:51:15 +0300

    задание 1

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа запрашивает натуральное число M, пользователь вводит с клавиатуры M чисел. Программа считает сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите натуральное число ");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] array_M = new int [M];
+int quantity = 0;
+for(int i=0; i<M; i++)
+   {
+      Console.WriteLine("введите число_" + i);
+      array_M[i] = Convert.ToInt32(Console.ReadLine());
+      if (array_M[i] > 0) 
+         quantity++;
+   }
+Console.WriteLine("Вы ввели " + quantity + " положительных чисел");
